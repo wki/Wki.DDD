a3a54697a092a8f97f08f66d21beada83648a2ec
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2015-02-11 20:10:35 +0100

    corrected extension method, bumped version to 1.0.3

--- Wki.DDD/EventBus/EventBusExtension.cs
+++ Wki.DDD/EventBus/EventBusExtension.cs
@@ -3,6 +3,7 @@
     public static class EventBusExtension
     {
         public static void Publish<T>(this IPublish obj, IEvent @event)
+            where T : class, IEvent
         {
             Hub.Current.Publish(@event);
         }
